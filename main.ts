86d259ee4142332680d904b46d4cdd6bdb783e08
inhadh-ai <inhadh@gmail.com>
2025-10-19 18:50:14 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+player.onChat("run", function () {
+    agent.place(FORWARD)
+})
+player.onTravelled(WALK, function () {
+	
+})
+player.onChat("run", function (num1) {
+	
+})
+agent.move(FORWARD, 1)
